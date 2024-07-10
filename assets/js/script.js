edcbe402b34377d38233098c31ad438bad6339ae
Justin-czk <115410317+Justin-czk@users.noreply.github.com>
2024-07-10 17:26:27 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 async function load() {
   const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbzge7cgF-I_tmvwWYZOp-GmLnzlTyaMqixBVKOnlIz-qNtpZsLRW44ioMjroI0OknvPug/exec"
+    "https://script.google.com/macros/s/AKfycbzge7cgF-I_tmvwWYZOp-GmLnzlTyaMqixBVKOnlIz-qNtpZsLRW44ioMjroI0OknvPug/exec", {
+      redirect: "follow",
+      method: "POST",
+      body: JSON.stringify(DATA),
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8",
+      },
+    }
   );
   console.log(response);
   const events = await response.json();
