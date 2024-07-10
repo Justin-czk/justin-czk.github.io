e50c0569804c656c5ec81f677188f537b9fd7cf9
Justin-czk <115410317+Justin-czk@users.noreply.github.com>
2024-07-10 16:13:16 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ async function load() {
   const response = await fetch(
     "https://script.google.com/macros/s/AKfycbzge7cgF-I_tmvwWYZOp-GmLnzlTyaMqixBVKOnlIz-qNtpZsLRW44ioMjroI0OknvPug/exec"
   );
-  //   console.log(response);
+  console.log(response);
   const events = await response.json();
   console.log({ events });
 }
