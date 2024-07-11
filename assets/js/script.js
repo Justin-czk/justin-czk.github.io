2a92fc3a1cd733ee4e8e685848b89d8e645ea63f
Justin-czk <115410317+Justin-czk@users.noreply.github.com>
2024-07-11 09:39:09 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 async function load() {
   const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbzge7cgF-I_tmvwWYZOp-GmLnzlTyaMqixBVKOnlIz-qNtpZsLRW44ioMjroI0OknvPug/exec", {
+    "https://script.google.com/macros/s/AKfycbzge7cgF-I_tmvwWYZOp-GmLnzlTyaMqixBVKOnlIz-qNtpZsLRW44ioMjroI0OknvPug/exec", 
+    {
       redirect: "follow",
       method: "GET",
-      body: JSON.stringify(DATA),
       headers: {
-        "Content-Type": "text/plain;charset=utf-8",
+        "Content-Type": "application/json",
       },
     }
   );
   console.log(response);
+  
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  
   const events = await response.json();
   console.log({ events });
 }
